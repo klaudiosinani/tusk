c4ad896243667c4ad4f22b66b70c9c58f32b9e24
Klaudio Sinani <klaudiosinani@protonmail.ch>
2017-08-13 20:06:16 +0300

    Auto-hiding menubar on Linux/Windows

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const Config = require('electron-config');
 module.exports = new Config({
   defaults: {
     zoomFactor: 1,
+    autoHideMenuBar: true,
     lastWindowState: {
       width: 800,
       height: 600
