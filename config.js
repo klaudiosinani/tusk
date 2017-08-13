152a36bc3d25907e88a3e709f0cd57a8346b6bf8
Klaus Sinani <klauscfhq@protonmail.com>
2017-08-13 20:17:12 +0300

    Updated minimum dimensions & minor tweaks

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ const Config = require('electron-config');
 module.exports = new Config({
   defaults: {
     zoomFactor: 1,
-    autoHideMenuBar: true,
     lastWindowState: {
       width: 800,
       height: 600
     },
+    alwaysOnTop: false,
     lastURL: 'https://www.evernote.com/Login.action'
   }
 });
