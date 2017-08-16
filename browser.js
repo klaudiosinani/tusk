0826d6fdbbdc60cd3ca8e79ae92e78c0c00457ef
Klaus Sinani <klauscfhq@protonmail.com>
2017-08-17 00:31:04 +0300

    Added `New Note` feature

--- browser.js
+++ browser.js
@@ -3,6 +3,11 @@ const electron = require('electron');
 
 const {ipcRenderer: ipc} = electron;
 
+ipc.on('new-note', () => {
+  // Create new note
+  document.querySelector('#gwt-debug-Sidebar-newNoteButton').click();
+});
+
 ipc.on('focus-mode', () => {
   // Toggle focus mode
   document.querySelector('#gwt-debug-NoteAttributes-focusButton').click();
