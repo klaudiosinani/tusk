e79e51dcd19d0e79d59178499485437e241e616d
Klaus Sinani <klauscfhq@protonmail.com>
2017-09-09 02:49:26 +0300

    Added update-notifying modal on Linux & Macos

--- update.js
+++ update.js
@@ -1,5 +1,55 @@
 'use strict';
 const electron = require('electron');
+const get = require('simple-get');
+
+const app = electron.app;
+const shell = electron.shell;
+const dialog = electron.dialog;
+const currentVersion = app.getVersion();
+const updateURL = 'https://champloohq.github.io/tusk/update.json';
+
+function checkUpdate(err, res, data) {
+  if (err) {
+    console.log('Update error.');
+  } else if (res.statusCode === 200) {
+    // Updating URL resolved properly
+    try {
+      data = JSON.parse(data); // Parse JSON safely
+    } catch (err) {
+      console.log('Invalid JSON object');
+    }
+    const latestVersion = data.version; // Get the latest version
+    console.log('Latest version of Tusk is: ' + latestVersion);
+    console.log('You are running Tusk on version: ' + currentVersion);
+    if (latestVersion === currentVersion) {
+      // User is already on the latest version
+      console.log('You are on the latest version');
+    } else {
+      // An updated version has been released
+      console.log('An update is available');
+      const result = dialog.showMessageBox({
+        type: 'info',
+        buttons: ['Download', 'Dismiss'],
+        defaultId: 0, // Make `Download` the default action button
+        title: 'Update Tusk',
+        message: 'Version ' + latestVersion + ' is now available',
+        detail: 'Click Download to get it now'
+      });
+      response(result); // Check which button was pressed based on index
+    }
+  } else {
+    // Updating URL did not resolve properly
+    console.log('Unexpected status code error');
+  }
+}
+
+function response(result) {
+  // If the `Download` button was pressed
+  // send the user to the latest Github release
+  if (result === 0) {
+    shell.openExternal('https://github.com/champloohq/tusk/releases/latest');
+  }
+}
 
 module.exports.init = () => {
   if (process.platform !== 'win32') {
@@ -32,9 +82,11 @@ module.exports.init = () => {
 };
 
 module.exports.checkUpdate = () => {
-  if (process.platform !== 'win32') {
-    return;
+  if (process.platform === 'win32') {
+    // Auto-update on Windows
+    electron.autoUpdater.checkForUpdates();
+  } else {
+    // Check for updates manually on Linux/Macos
+    get.concat(updateURL, checkUpdate);
   }
-
-  electron.autoUpdater.checkForUpdates();
 };
