7e8d7ed33401db1de013b75be5a2a5d84bfca0c1
Mario Sinani <mariosinani@protonmail.ch>
2017-10-29 02:03:42 +0300

    Ability to auto-launch app on login

---
+++ startup.js
@@ -0,0 +1,35 @@
+'use strict';
+const electron = require('electron');
+const AutoLaunch = require('auto-launch');
+
+const app = electron.app;
+
+const launchTusk = new AutoLaunch({
+  name: 'Tusk',
+  path: (process.platform === 'darwin') ? app.getPath('exe').replace(/\.app\/Content.*/, '.app') : undefined,
+  isHidden: true
+});
+
+function activate() {
+  // Activate app launch on login
+  return launchTusk
+  .isEnabled()
+  .then(enabled => {
+    if (!enabled) {
+      return launchTusk.enable();
+    }
+  });
+}
+
+function deactivate() {
+  // Deaactivate app launch on login
+  return launchTusk
+  .isEnabled()
+  .then(enabled => {
+    if (enabled) {
+      return launchTusk.disable();
+    }
+  });
+}
+
+module.exports = {activate, deactivate};
