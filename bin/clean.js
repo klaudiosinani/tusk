6edeaf16c9e0f2a6318e97d699130d8e58a41794
Klaudio Sinani <klaudiosinani@protonmail.ch>
2017-11-17 20:53:25 +0200

    Clean-up tweaks

--- bin/clean.js
+++ bin/clean.js
@@ -1,5 +1,25 @@
 'use strict';
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
 const rimraf = require('rimraf');
 
-// Remove compiled & packaged files
-rimraf.sync('dist/');
+const resolve = path.resolve;
+
+const green = chalk.bold.green;	// Green bold text
+const yellow = chalk.bold.yellow;	// Yellow bold text
+
+const distPath = resolve(__dirname, '../dist');	// Dist directory
+
+if (fs.existsSync(distPath)) {
+	// Check if the dist directory exists
+	try {
+		// Clean-up
+		rimraf.sync(distPath);
+		console.log(green('✔ Cleaning up'));
+	} catch (err) {
+		console.error(err);
+	}
+} else {
+	console.log(yellow('Nothing to clean-up'));
+}
