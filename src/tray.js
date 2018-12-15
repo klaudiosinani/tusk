0683fd0b901382d683ab894f4d496df938bf03d6
Klaus Sinani <klauscfhq@protonmail.com>
2018-12-15 19:45:56 +0200

    Refactored `tray` module

--- src/tray.js
+++ src/tray.js
@@ -1,128 +1,114 @@
 'use strict';
-const path = require('path');
-const electron = require('electron');
-const config = require('./config');
+const {app, Menu, shell, Tray} = require('electron');
+const {is} = require('./util');
+const settings = require('./settings');
+const file = require('./file');
+const url = require('./url');
+const win = require('./win');
 
-const {app, BrowserWindow, shell} = electron;
 let tray = null;
-const settingsURL = 'https://www.evernote.com/Settings.action';
-const issueURL = 'https://github.com/klaussinani/tusk/issues/new';
 
-function activate(command) {
-  const appWindow = BrowserWindow.getAllWindows()[0];
-  appWindow.show();
-  appWindow.webContents.send(command);
-}
-
-exports.create = win => {
-  if (process.platform === 'darwin' || tray) {
+function create() {
+  if (is.darwin || tray) {
     return;
   }
 
-  const iconPath = path.join(__dirname, '../static/IconTray.png');
-
-  const toggleWin = () => {
-    if (win.isVisible()) {
-      win.hide();
-    } else {
-      win.show();
-    }
-  };
-
-  const showWin = () => {
-    if (!win.isVisible()) {
-      win.show();
-    }
-  };
-
-  const contextMenu = electron.Menu.buildFromTemplate([{
-    label: 'Open Tusk',
-    click() {
-      toggleWin();
-    }
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Search',
-    click() {
-      showWin();
-      activate('search');
-    }
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Create',
-    submenu: [{
-      label: 'New Tag',
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Open Tusk',
       click() {
-        showWin();
-        activate('new-tag');
+        win.toggle();
       }
     }, {
-      label: 'New Note',
+      type: 'separator'
+    }, {
+      label: 'Search',
       click() {
-        showWin();
-        activate('new-note');
+        win.appear();
+        win.activate('search');
       }
     }, {
-      label: 'New Notebook',
-      click() {
-        showWin();
-        activate('new-notebook');
-      }
-    }]
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Toggle Theme',
-    submenu: [{
-      label: 'Sepia Theme',
-      click() {
-        showWin();
-        activate('toggle-sepia-mode');
+      type: 'separator'
+    }, {
+      label: 'Create',
+      submenu: [
+        {
+          label: 'New Tag',
+          click() {
+            win.appear();
+            win.activate('new-tag');
+          }
+        }, {
+          label: 'New Note',
+          click() {
+            win.appear();
+            win.activate('new-note');
+          }
+        }, {
+          label: 'New Notebook',
+          click() {
+            win.appear();
+            win.activate('new-notebook');
+          }
+        }
+      ]
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Toggle Theme',
+      submenu: [
+        {
+          label: 'Sepia Theme',
+          click() {
+            win.appear();
+            win.activate('toggle-sepia-mode');
+          }
+        }, {
+          label: 'Dark Theme',
+          click() {
+            win.appear();
+            win.activate('toggle-dark-mode');
+          }
+        }, {
+          label: 'Black Theme',
+          click() {
+            win.appear();
+            win.activate('toggle-black-mode');
+          }
+        }
+      ]
+    }, {
+      label: 'Auto Night Mode',
+      type: 'checkbox',
+      checked: settings.get('autoNightMode'),
+      click(item) {
+        win.appear();
+        settings.set('autoNightMode', item.checked);
+        win.activate('auto-night-mode');
       }
     }, {
-      label: 'Dark Theme',
+      type: 'separator'
+    }, {
+      label: 'Evernote Settings',
       click() {
-        showWin();
-        activate('toggle-dark-mode');
+        shell.openExternal(url.settings);
       }
     }, {
-      label: 'Black Theme',
+      label: 'Report Issue',
       click() {
-        showWin();
-        activate('toggle-black-mode');
+        shell.openExternal(url.issue);
       }
-    }]
-  }, {
-    label: 'Auto Night Mode',
-    type: 'checkbox',
-    checked: config.get('autoNightMode'),
-    click(item) {
-      showWin();
-      config.set('autoNightMode', item.checked);
-      activate('auto-night-mode');
+    }, {
+      type: 'separator'
+    }, {
+      role: 'quit'
     }
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Evernote Settings',
-    click() {
-      shell.openExternal(settingsURL);
-    }
-  }, {
-    label: 'Report Issue',
-    click() {
-      shell.openExternal(issueURL);
-    }
-  }, {
-    type: 'separator'
-  }, {
-    role: 'quit'
-  }]);
+  ]);
 
-  tray = new electron.Tray(iconPath);
+  tray = new Tray(file.trayIcon);
   tray.setToolTip(`${app.getName()}`);
-  tray.setContextMenu(contextMenu);
-  tray.on('click', toggleWin);
-};
+  tray.setContextMenu(menu);
+  tray.on('click', win.toggle);
+}
+
+module.exports = {create};
