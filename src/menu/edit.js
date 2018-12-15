3c9cb3190565b76216d900a2d82ce91e9b325625
Klaus Sinani <klauscfhq@protonmail.com>
2018-12-15 19:49:33 +0200

    Added `edit` submenu module

---
+++ src/menu/edit.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  label: 'Edit',
+  submenu: [
+    {
+      type: 'separator'
+    }, {
+      role: 'undo'
+    }, {
+      role: 'redo'
+    }, {
+      type: 'separator'
+    }, {
+      role: 'cut'
+    }, {
+      role: 'copy'
+    }, {
+      role: 'paste'
+    }, {
+      role: 'pasteandmatchstyle'
+    }, {
+      role: 'delete'
+    }, {
+      role: 'selectall'
+    }
+  ]
+};
