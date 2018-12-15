23db0f3c77257839ad6df4dca0fce41dff82b4c9
Klaus Sinani <klauscfhq@protonmail.com>
2018-12-15 19:45:09 +0200

    Added `settings` module

---
+++ src/settings.js
@@ -0,0 +1,32 @@
+'use strict';
+const settings = require('electron-settings');
+const {touchFileSync} = require('./util');
+
+touchFileSync(settings.file());
+
+settings.setAll({
+  alwaysOnTop: settings.get('alwaysOnTop', false),
+  autoLaunch: settings.get('autoLaunch', false),
+  autoNightMode: settings.get('autoNightMode', false),
+  hideTray: settings.get('hideTray', false),
+  lastWindowState: {
+    x: settings.get('lastWindowState.x'),
+    y: settings.get('lastWindowState.y'),
+    width: settings.get('lastWindowState.width', 900),
+    height: settings.get('lastWindowState.height', 500)
+  },
+  launchMinimized: settings.get('launchMinimized', false),
+  menuBarHidden: settings.get('menuBarHidden', false),
+  mode: {
+    black: settings.get('mode.black', false),
+    dark: settings.get('mode.dark', false),
+    sepia: settings.get('mode.sepia', false)
+  },
+  sideBarHidden: settings.get('sideBarHidden', false),
+  updateCheckPeriod: settings.get('updateCheckPeriod', 4),
+  useGlobalShortcuts: settings.get('useGlobalShortcuts', false),
+  useYinxiang: settings.get('useYinxiang', false),
+  zoomFactor: settings.get('zoomFactor', 1)
+});
+
+module.exports = settings;
