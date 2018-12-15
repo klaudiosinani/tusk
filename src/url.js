8bf062537c3689fc36e9ff7e76340affac4da55f
Klaudio Sinani <klaudiosinani@protonmail.ch>
2018-12-15 19:25:25 +0200

    Added `url` module. Fixes #130

---
+++ src/url.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  community: 'https://gitter.im/klaussinani/tusk',
+  evernote: 'https://www.evernote.com/Login.action',
+  homepage: 'https://klaussinani.github.io/tusk',
+  issue: 'https://github.com/klaussinani/tusk/issues/new',
+  keyboardShortcutsRef: 'https://github.com/klaussinani/tusk#keyboard-shortcuts',
+  license: 'https://github.com/klaussinani/tusk/blob/master/license.md',
+  redirect: 'https://www.evernote.com/OutboundRedirect.action?dest=',
+  release: 'https://github.com/klaussinani/tusk/releases/latest',
+  search: 'https://github.com/search?q=+is:issue+repo:klaussinani/tusk',
+  searchFeatureRequests: 'https://github.com/klaussinani/tusk/labels/feature-request',
+  settings: 'https://www.evernote.com/Settings.action',
+  source: 'https://github.com/klaussinani/tusk',
+  update: 'https://klaussinani.github.io/tusk/update.json',
+  yinxiang: 'https://app.yinxiang.com/Login.action'
+};
