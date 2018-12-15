240d1fafcc644ad8771b017945bd9f53e0ad812c
Klaudio Sinani <klaudiosinani@protonmail.ch>
2018-12-15 19:39:11 +0200

    Refactored `config` module

--- src/config.js
+++ src/config.js
@@ -1,33 +1,25 @@
 'use strict';
-const fs = require('fs-extra');
-const settings = require('electron-settings');
+const {readFileSync} = require('fs');
+const {ensureFileSync} = require('./util');
+const defaultConfig = require('./configs');
+const file = require('./file');
 
-fs.ensureFileSync(settings.file());
+const {log} = console;
 
-settings.setAll({
-  zoomFactor: settings.get('zoomFactor', 1),
-  lastWindowState: {
-    x: settings.get('lastWindowState.x'),
-    y: settings.get('lastWindowState.y'),
-    width: settings.get('lastWindowState.width', 900),
-    height: settings.get('lastWindowState.height', 500)
-  },
-  menuBarHidden: settings.get('menuBarHidden', false),
-  sideBarHidden: settings.get('sideBarHidden', false),
-  launchMinimized: settings.get('launchMinimized', false),
-  autoNightMode: settings.get('autoNightMode', false),
-  autoLaunch: settings.get('autoLaunch', false),
-  hideTray: settings.get('hideTray', false),
-  defaultMode: settings.get('defaultMode', true),
-  darkMode: settings.get('darkMode', false),
-  blackMode: settings.get('blackMode', false),
-  sepiaMode: settings.get('sepiaMode', false),
-  vibrantMode: settings.get('vibrantMode', false),
-  vibrantDarkMode: settings.get('vibrantDarkMode', false),
-  alwaysOnTop: settings.get('alwaysOnTop', false),
-  useYinxiang: settings.get('useYinxiang', false),
-  updateCheckPeriod: settings.get('updateCheckPeriod', '2h'),
-  useGlobalShortcuts: settings.get('useGlobalShortcuts', false)
-});
+class Config {
+  get shortcutKeys() {
+    return this._load.shortcutKeys;
+  }
 
-module.exports = settings;
+  get _load() {
+    ensureFileSync(file.localConfig, JSON.stringify(defaultConfig, null, 4));
+
+    try {
+      return JSON.parse(readFileSync(file.localConfig, 'utf8'));
+    } catch (error) {
+      return log(error);
+    }
+  }
+}
+
+module.exports = new Config();
