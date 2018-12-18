9376c47bc9123a984dac350bbd33e0e4452f180e
Klaudio Sinani <klaudiosinani@protonmail.ch>
2018-12-18 20:25:01 +0200

    Automatically infer default window dimensions from screen size

--- src/settings.js
+++ src/settings.js
@@ -12,8 +12,8 @@ settings.setAll({
   lastWindowState: {
     x: settings.get('lastWindowState.x'),
     y: settings.get('lastWindowState.y'),
-    width: settings.get('lastWindowState.width', 900),
-    height: settings.get('lastWindowState.height', 500)
+    width: settings.get('lastWindowState.width'),
+    height: settings.get('lastWindowState.height')
   },
   launchMinimized: settings.get('launchMinimized', false),
   menuBarHidden: settings.get('menuBarHidden', false),
