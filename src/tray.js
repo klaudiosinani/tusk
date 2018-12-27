9d8bbec105d1d57b7dd431c2dcdc5238eb46984d
Klaudio Sinani <klaudiosinani@protonmail.ch>
2018-12-27 23:57:59 +0200

    Minor tweaks on `tray` module

--- src/tray.js
+++ src/tray.js
@@ -1,118 +1,27 @@
 'use strict';
-const {app, Menu, shell, Tray} = require('electron');
+const electron = require('electron');
 const {is} = require('./util');
-const dialog = require('./dialog');
 const file = require('./file');
-const settings = require('./settings');
-const url = require('./url');
+const tpl = require('./menu/tray');
 const win = require('./win');
 
-let tray = null;
+const {app, Menu} = electron;
 
-function create() {
-  if (is.darwin || tray) {
-    return;
+class Tray {
+  constructor() {
+    this._tray = null;
   }
 
-  const menu = Menu.buildFromTemplate([
-    {
-      label: 'Open Tusk',
-      click() {
-        win.toggle();
-      }
-    }, {
-      type: 'separator'
-    }, {
-      label: 'Search',
-      click() {
-        win.appear();
-        win.activate('search');
-      }
-    }, {
-      type: 'separator'
-    }, {
-      label: 'Create',
-      submenu: [
-        {
-          label: 'New Tag',
-          click() {
-            win.appear();
-            win.activate('new-tag');
-          }
-        }, {
-          label: 'New Note',
-          click() {
-            win.appear();
-            win.activate('new-note');
-          }
-        }, {
-          label: 'New Notebook',
-          click() {
-            win.appear();
-            win.activate('new-notebook');
-          }
-        }
-      ]
-    }, {
-      type: 'separator'
-    }, {
-      label: 'Toggle Theme',
-      submenu: [
-        {
-          label: 'Sepia Theme',
-          click() {
-            win.appear();
-            win.activate('toggle-sepia-mode');
-          }
-        }, {
-          label: 'Dark Theme',
-          click() {
-            win.appear();
-            win.activate('toggle-dark-mode');
-          }
-        }, {
-          label: 'Black Theme',
-          click() {
-            win.appear();
-            win.activate('toggle-black-mode');
-          }
-        }
-      ]
-    }, {
-      label: 'Auto Night Mode',
-      type: 'checkbox',
-      checked: settings.get('autoNightMode'),
-      click(item) {
-        win.appear();
-        settings.set('autoNightMode', item.checked);
-        win.activate('auto-night-mode');
-      }
-    }, {
-      type: 'separator'
-    }, {
-      label: 'Evernote Settings',
-      click() {
-        shell.openExternal(url.settings);
-      }
-    }, {
-      label: 'Report Issue',
-      click() {
-        shell.openExternal(url.issue);
-      }
-    }, {
-      type: 'separator'
-    }, {
-      label: 'Exit',
-      click() {
-        dialog.confirmExit();
-      }
+  create() {
+    if (is.darwin) {
+      return;
     }
-  ]);
 
-  tray = new Tray(file.trayIcon);
-  tray.setToolTip(`${app.getName()}`);
-  tray.setContextMenu(menu);
-  tray.on('click', win.toggle);
+    this._tray = new electron.Tray(file.trayIcon);
+    this._tray.setToolTip(app.getName());
+    this._tray.setContextMenu(Menu.buildFromTemplate(tpl));
+    this._tray.on('click', win.toggle);
+  }
 }
 
-module.exports = {create};
+module.exports = new Tray();
