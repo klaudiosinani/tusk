dd03d0639296fd79b5fb9c5b241c6b0764b9c8e3
DX.Kevin <33936992+DX-Kevin@users.noreply.github.com>
2022-10-10 00:00:16 +0800

    Update url.js

--- src/url.js
+++ src/url.js
@@ -2,17 +2,17 @@
 
 module.exports = {
   community: 'https://gitter.im/klaussinani/tusk',
-  evernote: 'https://www.evernote.com/Login.action',
+  evernote: 'https://app.yinxiang.com/Login.action',
   homepage: 'https://klaussinani.github.io/tusk',
   issue: 'https://github.com/klaussinani/tusk/issues/new',
   keyboardShortcutsRef: 'https://github.com/klaussinani/tusk#keyboard-shortcuts',
   license: 'https://github.com/klaussinani/tusk/blob/master/license.md',
-  redirect: 'https://www.evernote.com/OutboundRedirect.action?dest=',
-  release: 'https://github.com/klaussinani/tusk/releases/latest',
+  redirect: 'https://app.yinxiang.com/OutboundRedirect.action?dest=',
+  release: 'https://github.com/DX-Kevin/tusk/releases/latest',
   search: 'https://github.com/search?q=+is:issue+repo:klaussinani/tusk',
   searchFeatureRequests: 'https://github.com/klaussinani/tusk/labels/feature-request',
-  settings: 'https://www.evernote.com/Settings.action',
-  source: 'https://github.com/klaussinani/tusk',
+  settings: 'https://app.yinxiang.com/Settings.action',
+  source: 'https://github.com/DX-Kevin/tusk',
   update: 'https://raw.githubusercontent.com/klaussinani/tusk/master/docs/update.json',
   yinxiang: 'https://app.yinxiang.com/Login.action',
   yinxiangRedirect: 'https://app.yinxiang.com/OutboundRedirect.action?dest='
